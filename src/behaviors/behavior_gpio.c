b96802cff9aacc232ff02d4b661ccddb666a6300
elpekenin <elpekenin@elpekenin.dev>
2024-09-19 21:49:04 +0200

    more logging

--- src/behaviors/behavior_gpio.c
+++ src/behaviors/behavior_gpio.c
@@ -29,22 +29,21 @@ static const struct gpio_dt_spec gpios[] = {
 #define N_GPIO ARRAY_SIZE(gpios)
 
 static int behavior_gpio_init(const struct device *dev) {
-    for (
-        const struct gpio_dt_spec *gpio = gpios;
-        gpio < &gpios[N_GPIO];
-        ++gpio
-    ) {
+    for (uint8_t i = 0; i < N_GPIO, ++i) {
+        const struct gpio_dt_spec *gpio = &gpios[i];
+
         // initialize pin
         if (
             !gpio_is_ready_dt(gpio)
             || gpio_pin_configure_dt(gpio, GPIO_OUTPUT_ACTIVE) != 0
         ) {
-            LOG_ERR("gpio (%s) did not cooperate: extermine", dev->name);
+            LOG_ERR("gpio %d did not cooperate: extermine it", i);
             return -ENODEV;
         }
 
         // ensure LED is off
         gpio_pin_set_dt(gpio, false);
+        LOG_DBG("gpio %d initialized and turned off", i);
     }
 
     return 0;
@@ -58,7 +57,7 @@ static int on_keymap_binding_pressed(
         binding->param2 < 0
         && binding->param2 > GPIO_TOG
     ) {
-        LOG_ERR("Invalid state (%d) for gpio.", binding->param2);
+        LOG_ERR("Invalid state (%d) for &gpio behavior", binding->param2);
         return -ENOTSUP;
     }
 
@@ -67,14 +66,16 @@ static int on_keymap_binding_pressed(
         binding->param1 < 0
         || binding->param1 >= N_GPIO
     ) {
-        LOG_ERR("Invalid gpio number (%d).", binding->param1);
+        LOG_ERR("Invalid gpio number %d for &gpio behavior", binding->param1);
         return -ENOTSUP;
     }
 
     if (binding->param2 == GPIO_TOG) {
+        LOG_DBG("gpio %d toggled", binding->param1);
         return gpio_pin_toggle_dt(&gpios[binding->param1]);
     }
 
+    LOG_DBG("gpio %d turned %s", binding->param1, (binding->param2 == GPIO_ON) ? "on" : "off");
     return gpio_pin_set_dt(&gpios[binding->param1], binding->param2);
 }
 
@@ -82,6 +83,7 @@ static int on_keymap_binding_released(
     struct zmk_behavior_binding *binding,
     struct zmk_behavior_binding_event event
 ) {
+    LOG_DBG("ignoring key release");
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
